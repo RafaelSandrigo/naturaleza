f32170455ad2ef5e980d88caca1beca79cd87215
Rafael Sandrigo <rafaelsandrigo1513@gmail.com>
2024-11-20 13:33:11 -0300

    feat(api): Criação da API de comunicados

---
+++ app/Http/Controllers/Modulos/ApiComunicadosController.php.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers\Modulos;
+
+use App\Http\Controllers\Controller;
+use App\Models\Comunicados;
+use Illuminate\Http\Request;
+
+class ApiComunicadosController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Comunicados  $comunicados
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Comunicados $comunicados)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Comunicados  $comunicados
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Comunicados $comunicados)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Comunicados  $comunicados
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Comunicados $comunicados)
+    {
+        //
+    }
+}
